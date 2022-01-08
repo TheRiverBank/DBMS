935d5493820127cc7c11b552364df53b27eaa3d0
TheRiverBank <sondre_lovas@hotmail.com>
2022-01-08 22:59:42 +0100

    paging updates

--- bored.c
+++ bored.c
@@ -19,25 +19,39 @@ typedef struct int_item int_item_t;
 typedef struct str_item str_item_t;
 
 int insert_record() {
+    /* Write something to page 1 and close the file */
     const char *filename = "db";
-    open_file(filename);
-
+    
+    printf("Opening file\n");
+    int fd = open_file(filename);
+    printf("File open.\nCreating page\n");
     page_t p = get_page(filename, 0);
+    printf("Page created\nSetting pos\n");
     page_set_current_pos(filename, 12, p);
-    page_put_int(filename, 12, p);
+    printf("Pos set\nPlacing int\n");
+    page_put_int(filename, 69, p);
     int val = page_get_int(filename, p);
 
+    printf("page 0 val: %d\n", val);
+    write_page(filename, p);
+    close_file(fd);
+}
 
-    page_t p_2 = get_page(filename, 2);
-    page_set_current_pos(filename, 12, p_2);
-    page_put_int(filename, 123, p_2);
-    int val_2 = page_get_int(filename, p_2);
+int retreive_record() {
+    const char *filename = "db";
+    int fd = open_file(filename);
+
+    page_t p1;
+    read_page(filename, p1);
+
+    page_set_current_pos(filename, 12, p1);
 
 
-    page_set_current_pos(filename, 12, p);
-    val = page_get_int(filename, p);
+    int val1 = page_get_int(filename, p1);
+  
 
-    printf("page 1 val: %d, page 2 val: %d\n", val, val_2);
+    printf("page 0 val: %d\n", val1);  
+
 }
 
 int main() {
@@ -46,4 +60,5 @@ int main() {
     open_file("db");
 
     insert_record();
+    retreive_record();
 }
