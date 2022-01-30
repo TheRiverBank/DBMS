02f73c1302227d13459c8b696c458b0d4dacd485
TheRiverBank <sondre_lovas@hotmail.com>
2022-01-30 16:46:11 +0100

    seq index file

---
+++ sequential_index_file.c
@@ -0,0 +1,7 @@
+/* B+-tree index file */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "sequential_index_file.h"
+
---
+++ sequential_index_file.h
@@ -0,0 +1,5 @@
+struct file_entry {
+    int search_key;
+    int record_addr;
+};
+typedef struct file_entry file_entry_t;
